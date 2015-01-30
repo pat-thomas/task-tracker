932dd1852642c7dbedd79c8cf225fe5f3d9f98ed
Pat Thomas <pthomas@relaynetwork.com>
2015-01-29 22:56:01 -0500

    Helper code to load service config.

---
+++ src/task_tracker/server/service_config.clj
@@ -0,0 +1,12 @@
+(ns task-tracker.server.service-config
+  (:require [clojure.data.json :as json]))
+
+(def config-atom (atom nil))
+
+(defn load-config!
+  [config-file-path]
+  (reset! config-atom (-> config-file-path slurp json/read-json)))
+
+(defn config
+  [& ks]
+  (get-in @config-atom ks))
